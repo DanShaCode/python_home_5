53d1fa8ca3ed790a53cd726a0a5ab25aa318507b
Danis Sharipov <danshadance@mail.ru>
2023-02-09 02:29:27 +0600

    task2 upd beta version

--- task2.py
+++ task2.py
@@ -13,53 +13,72 @@ import os
 import time
 
 def player1 (candy_amount):
-
-
-def Game (candy_amount):
     os.system('cls||clear')
-    print("Начало нового раунда!")
-    flag = 0
     if candy_amount > 0:
         print()
         print("Количество конфет на столе: ", candy_amount)
         print()
-        flag += 1
         player_1 = int(input("Ход Игрока_1. Введите количество конфет: "))
     if player_1 < 1 or player_1 > 500:
         print()
         print("Количество конфет, которые вы берете не может быть меньше 0 или больше 28!")
+        temp = candy_amount
+        candy_amount = 0
         time.sleep(2)
+        player1(temp)
     else:
         candy_amount -= player_1
+    if candy_amount <= 0:
+        return Game(candy_amount - 1)
+    elif candy_amount > 0:
         print()
         print("Количество конфет на столе: ", candy_amount)
         print()
+        player2(candy_amount)
+
+def player2 (candy_amount):
+    os.system('cls||clear')
     if candy_amount > 0:
-        os.system('cls||clear')
-        print("Начало нового раунда!")
         print()
         print("Количество конфет на столе: ", candy_amount)
         print()
-        flag -= 1
         player_2 = int(input("Ход Игрока_2. Введите количество конфет: "))
     if player_2 < 1 or player_2 > 500:
         print()
         print("Количество конфет, которые вы берете не может быть меньше 0 или больше 28!")
+        temp = candy_amount
+        candy_amount = 0
         time.sleep(2)
-        error2 += 1
-    elif error2 == 0:
+        player2(temp)
+    else:
         candy_amount -= player_2
-    if candy_amount <= 0:
-        if flag == 0:
-            print()
-            print("Конец игры. Игрок_2 выйграл!")
-        else:
-            print("Конец игры. Игрок_1 выйграл!")
-    elif candy_amount > 0:
+    if candy_amount > 0:
+        print()
+        print("Количество конфет на столе: ", candy_amount)
+        print()
         Game(candy_amount)
+    elif candy_amount <= 0:
+        return Game(candy_amount - 2)
+
+def Game (candy_amount):
+    os.system('cls||clear')
+    if candy_amount > 0:
+        player1(candy_amount)
+    elif candy_amount <= 0 and candy_amount - 1:
+        print()
+        print("Игра окончена!")
+        print()
+        print("Игрок_1 победил!")
+    elif candy_amount <= 0 and candy_amount - 2:
+        print()
+        print("Игра окончена!")
+        print()
+        print("Игрок_2 победил!")
+
+print()
+print("Иигра началась!")
+print()
+
+time.sleep(1)
 
 Game(2021)
-
-    
-
-
