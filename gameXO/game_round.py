f36cb89cd0f49bf0f837f4c3e0f6fb414d112b63
Danis Sharipov <danshadance@mail.ru>
2023-02-10 23:58:04 +0600

    realise_Game_XO_v1.0.0

--- gameXO/game_round.py
+++ gameXO/game_round.py
@@ -8,10 +8,17 @@ def Game_Round(start_board):
     Board_Print(start_board)
     first_board = First_Player(start_board)
     first_check = First_Win_Check(first_board)
+    no_win_check_first = No_Win(first_board)
     if first_check == True:
         Board_Print(first_board)
         print()
         print("Игрок_1 победил!")
+        time.sleep(2)
+        os.system('cs||clear')
+    if no_win_check_first == True:
+        Board_Print(first_board)
+        print()
+        print("Ничья!")
         time.sleep(2)
         os.system('cs||clear')
     else:
@@ -19,11 +26,18 @@ def Game_Round(start_board):
         Board_Print(first_board)
         second_board = Second_PLayer(first_board)
         second_check = Second_Win_Check(second_board)
+        no_win_check_second = No_Win(second_board)
         if second_check == True:
             Board_Print(second_board)
             print()
             print("Игрок_2 победил!")
             time.sleep(2)
             os.system('cs||clear')
+        if no_win_check_second == True:
+            Board_Print(first_board)
+            print()
+            print("Ничья!")
+            time.sleep(2)
+            os.system('cs||clear')    
         elif second_check == False:
             Game_Round(second_board)
